c0953d5b7f7fc242ec505c363c60a0996bad0314
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-01-20 21:50:02 -0300

    First directives

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>{{title}}</h1>
+  <h1 v-text="text"></h1>
+  <!-- <h1 v-show="isShowing"></h1> -->
+  <!-- <h1 v-if="isShowing" v-html="title"></h1>
+  <h3 v-else-if="isShowing === null">This is v-else-if</h3>
+  <h2 v-else>This is v-else part</h2> -->
   <p>Something else</p>
 </template>
 
@@ -7,7 +11,10 @@
 export default {
   data(){
     return {
-      title: "Vue3!",
+      title: "<h1>Hey</h1>",
+      text: "Text",
+      isShowing: false,
+      count: 0,
     }
   }
 }
