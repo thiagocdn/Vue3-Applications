3419201842e9b299d4c3ebdc1885f967aba49bf5
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-01-21 21:05:37 -0300

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <h1>Dc Heroes</h1>
+  <h1>Dc Heroes: {{ heroesCount }}</h1>
   <ul>
-    <li v-for="(heroe, index) in dcHeroes" v-bind:key="index">{{index}} - {{heroe.name}}</li>
+    <li v-for="(heroe, index) in dcHeroes" :key="index">
+      {{heroe.name}}
+    </li>
   </ul>
+  <form @submit.prevent="addHero">
+    <input v-model.trim="newHero" placeholder="Type Hero name">
+    <button type="submit">Add Hero</button>
+  </form>
+  <small>Total heroes in the list is {{heroesCount}}</small>
 </template>
 
 <script>
 export default {
+  computed: {
+    heroesCount() {
+      return this.dcHeroes.length;
+    }
+  },
+  methods: {
+    addHero() {
+      if(this.newHero !== '') {
+        this.dcHeroes.push({ name: this.newHero });
+        this.newHero='';
+      }
+    },
+    removeHero() {
+
+    },
+  },
   data() {
     return {
+      newHero: "",
       dcHeroes: [
         { name: 'SuperGirl' },
         { name: 'Flash' },
         { name: 'Batman' },
         { name: 'Arrow' }
-      ]
+      ],
     }
   }
 
